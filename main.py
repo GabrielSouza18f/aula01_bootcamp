cacbc5d74a294ed3aa5ef12eaaa70455ec9e6851
Desenvolvimento <gabriel.ferreirat218@gmail.com>
2024-08-04 18:49:34 -0300

    Atualização do arquivo

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print("Hello World")
 print("Hello World")
+
+input("informe seu cpf: ")
+
+# crie programa para o usuario digitar o seu nome e os caracteres
+
+print(len(input('Digite o seu nome: ')))
+
+
+valor = int(input("Digite o valor"))
+valor1 = int(input("Digite o valor"))
+
+print(valor + valor1)
